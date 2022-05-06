98264797855907984411ede76e27bc8c7027c674
ellaempire <99174835+ellaempire@users.noreply.github.com>
2022-05-06 08:49:12 +0100

    Add files via upload

---
+++ EllaXPython/c0de 001.py
@@ -0,0 +1,13 @@
+print ("welcome")
+f = input ("guess the number")
+guess = int(f)
+if guess == 100:
+  print (" you win ")
+else:
+    if guess > 5:
+          print ("too high")
+    else:
+          print ("too low, c'mon!")
+print ("GAME OVER")                 
+
+            
---
+++ EllaXPython/practice code.py
@@ -0,0 +1,10 @@
+# this program says hello and asks for my name
+print('heya')
+print('what is your name?') #ask for their name
+myName= input ()
+print('its good to meet you,' + myName)
+print('the lenght of your name is..')
+print(len(myName))
+print('how old are you') #ask for their age
+myAge= input()
+print('you will be' + str(int(myAge)+ 20) + 'in a year')
